4480fc8eaf8ceb72cb94b698cdf0843220322453
Adam <adammisiak3@gmail.com>
2021-07-19 19:20:18 +0200

    Added photo

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,12 +1,16 @@
 import '../styles/Home.css'
 
 import React from "react";
+import avatar from '../avatar.jpg';
 
 const Home: React.FunctionComponent = () => {
   return (
     <section id="home">
       <div className="home-wrapper">
-        <div className="home-sign">
+        <div className="home-photo-wrapper">
+          <img className="home-photo" src={avatar} alt="Avatar"></img>
+        </div>
+        <div className="home-sign-wrapper">
           HELLO, MY NAME IS ADAM
         </div>
       </div>
