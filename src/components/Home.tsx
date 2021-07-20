0c41b8729a63cce9acbeec4f7dbdc7f1b0dbba07
Adam <adammisiak3@gmail.com>
2021-07-20 16:25:58 +0200

    Added animation to home page

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,12 +1,25 @@
 import '../styles/Home.css'
 
-import React from "react";
+import React, { useEffect } from "react";
+
+import { init } from 'ityped';
 
 const Home: React.FunctionComponent = () => {
+  useEffect(() => {
+    const writer: any = document.querySelector("#writer");
+    init(writer, {
+      showCursor: true,
+      strings: ["Python", "Django", "Typescript"]
+    });
+  });
+
+  
   return (
     <section id="home">
       <div className="home-sign-wrapper">
-        <h1>HELLO, MY NAME IS ADAM</h1>
+        <h1>I am Adam Misiak</h1>
+        <p>Backend Developer located in Gdańsk.</p>
+        <p>Specialized in <span id="writer"></span></p>
       </div>
     </section>
   );
