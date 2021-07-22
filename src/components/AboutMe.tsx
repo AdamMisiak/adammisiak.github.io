307ae5917bd74c8de761f65a6a9f232265ec6921
Adam <adammisiak3@gmail.com>
2021-07-22 19:25:14 +0200

    Changed styles of about me section

--- src/components/AboutMe.tsx
+++ src/components/AboutMe.tsx
@@ -8,8 +8,9 @@ const AboutMe: React.FunctionComponent = () => {
     <section id="about-me">
       <div className="about-me-wrapper">
         <div className="about-me-text">
-          <h1>About Me</h1>
-          <p>Software Developer</p>
+          <h1 className="about-me-title">About Me</h1>
+          <p className="about-me-subtitle">Software Developer</p>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
         </div>
         <div className="about-me-photo-wrapper">
           <img className="about-me-photo" src={avatar} alt="Avatar"></img>
