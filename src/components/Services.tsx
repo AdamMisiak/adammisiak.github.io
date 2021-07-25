28e32c27b7a8214110dd147423a8d773764db8c5
Adam <adammisiak3@gmail.com>
2021-07-25 18:56:36 +0200

    Added services styles

--- src/components/Services.tsx
+++ src/components/Services.tsx
@@ -7,11 +7,19 @@ const Services: React.FunctionComponent = () => {
     <section id="services">
       <div className="services-wrapper">
         <div className="services-sign">
-            <h1>Services</h1>
-            <p>Thing that I specilize in</p>
+          <h1>Services</h1>
+          <p>Things that I specilize in:</p>
         </div>
-        <div className="services-list-wrapper">
-            
+        <div className="services-list">
+          <div className="services-element">
+            <h1>Frontend Development</h1>
+          </div>
+          <div className="services-element">
+            <h1>Backend Development</h1>
+          </div>
+          <div className="services-element">
+            <h1>Scripts Automation</h1>
+          </div>
         </div>
       </div>
     </section>
