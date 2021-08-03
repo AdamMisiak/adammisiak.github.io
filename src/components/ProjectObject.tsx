f709ae9b8cb6c1f5bdb365b2d0facb50bd05bd59
Adam <adammisiak3@gmail.com>
2021-08-03 21:01:10 +0200

    Added more projects

--- src/components/ProjectObject.tsx
+++ src/components/ProjectObject.tsx
@@ -5,14 +5,20 @@ import React from "react";
 interface ProjectObjectProps {
     title: string;
     image: string;
-    text: string;
+    link: string;
 }
 
 const ProjectObject: React.FunctionComponent<ProjectObjectProps> = (props: ProjectObjectProps) => {
     return (
       <div className="projects-element">
-        <h1>{props.title}</h1>
-        <p>{props.text}</p>
+        <a href={props.link} target="_blank" rel="noreferrer">
+          <div className="projects-photo-wrapper">
+              <img className="projects-photo" src={props.image} alt={props.title}></img>
+          </div>
+          <div className="projects-text-wrapper">
+              <h1>{props.title}</h1>
+          </div>
+        </a>
       </div>
     );
 };
--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -3,9 +3,33 @@ import '../styles/Projects.css'
 import ProjectObject from './ProjectObject';
 import React from "react";
 import blogs_manager from '../static/blogs_manager.jpg';
+import exchange_rates_calculator from '../static/exchange_rates_calculator.jpg';
+import tic_tac_toe_game from '../static/tic_tac_toe_game.jpg';
+import to_do_list from '../static/to_do_list.jpg';
 
 const Projects: React.FunctionComponent = () => {
-  
+  const projectsObjects = [
+    {
+      'title': 'Blogs Manager',
+      'image': blogs_manager,
+      'link': "https://github.com/AdamMisiak/Blogs_manager",
+    },
+    {
+      'title': 'To Do List',
+      'image': to_do_list,
+      'link': "https://github.com/AdamMisiak/To_do_list",
+    },
+    {
+      'title': 'Tic Tac Toe',
+      'image': tic_tac_toe_game,
+      'link': "https://github.com/AdamMisiak/Tic_tac_toe_game",
+    },
+    {
+      'title': 'Exchange rates calculator',
+      'image': exchange_rates_calculator,
+      'link': "https://github.com/AdamMisiak/Exchange_rates_calculator",
+    },
+  ]
   
   return (
     <section id="projects">
@@ -14,14 +38,9 @@ const Projects: React.FunctionComponent = () => {
           <h1>Projects</h1>
         </div>
         <div className="projects-list">
-            <div className="projects-element">
-                <div className="projects-photo-wrapper">
-                    <img className="projects-photo" src={blogs_manager} alt="Blogs Manager"></img>
-                </div>
-                <div className="projects-text-wrapper">
-                    <h1>Blogs Manager</h1>
-                </div>
-            </div>
+          {projectsObjects.map((object) => {
+            return <ProjectObject {...object} />
+          })}
         </div>
       </div>
     </section>
