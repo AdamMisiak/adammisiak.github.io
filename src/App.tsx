ac56bb7bfda8fd156e08f334c649f3afdb99c657
Adam <adammisiak3@gmail.com>
2021-08-04 20:25:06 +0200

    Added animation to sidebar

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
 import './styles/index.css'
+import "aos/dist/aos.css"
 
+import React, { useEffect } from "react";
+
+import Aos from 'aos';
 import Feed from './components/Feed';
-import React from 'react';
 import Sidebar from './components/Sidebar';
 
 const App: React.FunctionComponent = () => {
+  useEffect(() => {
+    Aos.init({duration: 1500});
+  })
   return <div className="App">
     <div className="wrapper">
       <Sidebar />
--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,19 +1,17 @@
 import '../styles/Sidebar.css'
 
-import React from "react";
-
 const Sidebar: React.FunctionComponent = () => {
   return (
     <section id="sidebar">
       <div className="sidebar-wrapper">
         <nav>
           <ul>
-            <li><a href="#home">HOME</a></li>
-            <li><a href="#about-me">ABOUT ME</a></li>
-            <li><a href="#services">SERVICES</a></li>
-            <li><a href="#skills">SKILLS</a></li>
-            <li><a href="#projects">PROJECTS</a></li>
-            <li><a href="#contact">CONTACT</a></li>
+            <li><a data-aos="fade-right" data-aos-duration="1000" href="#home">HOME</a></li>
+            <li><a data-aos="fade-right" data-aos-duration="1150" href="#about-me">ABOUT ME</a></li>
+            <li><a data-aos="fade-right" data-aos-duration="1300" href="#services">SERVICES</a></li>
+            <li><a data-aos="fade-right" data-aos-duration="1350" href="#skills">SKILLS</a></li>
+            <li><a data-aos="fade-right" data-aos-duration="1500" href="#projects">PROJECTS</a></li>
+            <li><a data-aos="fade-right" data-aos-duration="1650" href="#contact">CONTACT</a></li>
           </ul>
         </nav>
       </div>
