3cb78e4caa124514880beb6a4b8ec0554523e2d0
Adam <adammisiak3@gmail.com>
2021-08-16 22:53:12 +0200

    Changed timeline objects to components

--- src/components/AboutMe.tsx
+++ src/components/AboutMe.tsx
@@ -1,34 +1,46 @@
 import '../styles/AboutMe.css'
 
-import { Calendar } from '@styled-icons/boxicons-regular/Calendar';
-import { Server } from '@styled-icons/boxicons-regular/Server';
+import { Building } from '@styled-icons/bootstrap/Building';
 import TimelineObject from './TimelineObject';
+import { University } from '@styled-icons/fa-solid/University';
 import avatar from '../static/avatar.jpg';
 
 const timelineObjects = [
   {
+    'title': 'Junior Python Developer',
+    'icon': Building,
+    'company': 'iMakeable', 
     'date': 'May 2021 - present',
-    'title': 'Junior Python Developer @ iMakeable',
-    'text': "Technologies used: Python, Django, DRF, Postgres, Celery, Docker, Elasticsearch",
-    'time': '0',
+    'text': 'I was involved into social media app. Worked with frontend developer and DevOps. ',
+    'technologies': ['Python', 'Django', 'DRF', 'Postgres', 'Docker'],
+    'animationDelay': '0',
   },
   {
+    'title': 'Junior Python Developer',
+    'icon': Building,
+    'company': 'The BitByBit Company',
     'date': 'October 2020 - present',
-    'title': 'Junior Python Developer @ The BitByBit Company',
     'text': "Technologies used: Python, Django, DRF, Postgres, Docker",
-    'time': '200',
+    'technologies': ['Python', 'Django', 'DRF', 'Postgres', 'Celery', 'Docker', 'Elasticsearch'],
+    'animationDelay': '100',
   },
   {
+    'title': 'PLC Programmer',
+    'icon': Building,
+    'company': 'Pesmel Poland',
     'date': 'July 2019 - August 2020',
-    'title': 'PLC Programmer @ Pesmel Poland',
     'text': "Duties: coding in C#/Unity, programming PLC in TIA Portal, preparing visualizations",
-    'time': '400',
+    'technologies': ['PLC', 'C#', 'Unity', 'SCADA'],
+    'animationDelay': '100',
   },
   {
+    'title': 'Student',
+    'icon': University,
+    'company': 'Gdańsk University of Technology',
     'date': 'October 2016 - February 2020',
-    'title': 'Gdańsk University of Technology',
     'text': "Graduated with bachelor of engineering degree in Automatic Control and Robotics. Topic of my thesis was: Unmanned wheeled eletric vehicle. I was a part of student organisation SK SEP.",
-    'time': '600',
+    'technologies': ['PLC', 'SCADA', 'Automation'],
+    'animationDelay': '100',
   },
 ]
 
@@ -46,115 +58,9 @@ const AboutMe: React.FunctionComponent = () => {
         </div>
       </div>
       <div className="timeline-wrapper">
-        <div className="timeline-block">
-          <div className="timeline-orb">
-			    </div>
-          <div className="timeline-content">
-            <h2>Penta Consulting</h2>
-            <div className="timeline-content-info">
-              <span className="timeline-content-info-title">
-                <Server size="16px"/>&nbsp;
-                Backend Developer
-              </span>
-              <span className="timeline-content-info-date">
-                <Calendar size="16px"/>&nbsp;
-                June 2016 - Present
-              </span>
-            </div>
-            <p>Working alongside the designer team implementing the designs, also developing custom solutions to address team necessities.</p>
-            <ul className="timeline-content-skills">
-              <li>HTML5</li>
-              <li>CSS3</li>
-              <li>JavaScript</li>
-              <li>jQuery</li>
-              <li>Wordpress</li>
-            </ul>
-          </div>
-        </div>
-
-        <div className="timeline-block">
-          <div className="timeline-orb">
-			    </div>
-          <div className="timeline-content">
-            <h2>Penta Consulting</h2>
-            <div className="timeline-content-info">
-              <span className="timeline-content-info-title">
-                <i className="fa fa-certificate" aria-hidden="true"></i>
-                Front End Developer
-              </span>
-              <span className="timeline-content-info-date">
-                <i className="fa fa-calendar-o" aria-hidden="true"></i>
-                June 2016 - Present
-              </span>
-            </div>
-            <p>Working alongside the designer team implementing the designs, also developing custom solutions to address team necessities.</p>
-            <ul className="timeline-content-skills">
-              <li>HTML5</li>
-              <li>CSS3</li>
-              <li>JavaScript</li>
-              <li>jQuery</li>
-              <li>Wordpress</li>
-            </ul>
-          </div>
-        </div>
-
-        <div className="timeline-block">
-          <div className="timeline-orb">
-			    </div>
-          <div className="timeline-content">
-            <h2>Penta Consulting</h2>
-            <div className="timeline-content-info">
-              <span className="timeline-content-info-title">
-                <i className="fa fa-certificate" aria-hidden="true"></i>
-                Front End Developer
-              </span>
-              <span className="timeline-content-info-date">
-                <i className="fa fa-calendar-o" aria-hidden="true"></i>
-                June 2016 - Present
-              </span>
-            </div>
-            <p>Working alongside the designer team implementing the designs, also developing custom solutions to address team necessities.</p>
-            <ul className="timeline-content-skills">
-              <li>HTML5</li>
-              <li>CSS3</li>
-              <li>JavaScript</li>
-              <li>jQuery</li>
-              <li>Wordpress</li>
-            </ul>
-          </div>
-        </div>
-
-        <div className="timeline-block">
-          <div className="timeline-orb">
-			    </div>
-          <div className="timeline-content">
-            <h2>Penta Consulting</h2>
-            <div className="timeline-content-info">
-              <span className="timeline-content-info-title">
-                <i className="fa fa-certificate" aria-hidden="true"></i>
-                Front End Developer
-              </span>
-              <span className="timeline-content-info-date">
-                <i className="fa fa-calendar-o" aria-hidden="true"></i>
-                June 2016 - Present
-              </span>
-            </div>
-            <p>Working alongside the designer team implementing the designs, also developing custom solutions to address team necessities.</p>
-            <ul className="timeline-content-skills">
-              <li>HTML5</li>
-              <li>CSS3</li>
-              <li>JavaScript</li>
-              <li>jQuery</li>
-              <li>Wordpress</li>
-            </ul>
-          </div>
-        </div>
-        
-        {/* <ul>
-          {timelineObjects.map((object) => {
-            return <TimelineObject {...object} />
-          })}
-        </ul> */}
+        {timelineObjects.map((object) => {
+          return <TimelineObject {...object} />
+        })}
       </div>
     </section>
   );
--- src/components/TimelineObject.tsx
+++ src/components/TimelineObject.tsx
@@ -1,24 +1,45 @@
 import '../styles/AboutMe.css'
 
+import { Calendar } from '@styled-icons/boxicons-regular/Calendar';
 import React from "react";
+import { Server } from '@styled-icons/boxicons-regular/Server';
+import {StyledIcon} from '@styled-icons/styled-icon'
 
 interface TimelineObjectProps {
-    date: string;
     title: string;
+    icon: StyledIcon,
+    company: string,
+    date: string;
     text: string;
-    time: string;
+    technologies: string[];
+    animationDelay: string;
 }
 
 const TimelineObject: React.FunctionComponent<TimelineObjectProps> = (props: TimelineObjectProps) => {
     return (
-        // data-aos="fade-left" data-aos-delay={props.time}
-    <li>
-        <div className="timeline-content">
-            <h2 className="timeline-date">{props.date}</h2>
-            <h1>{props.title}</h1>
-            <p>{props.text}</p>
+      <div className="timeline-block">
+        <div className="timeline-orb"></div>
+        <div data-aos="fade-left" data-aos-delay={props.animationDelay} className="timeline-content">
+          <h2>{props.title}</h2>
+          <div className="timeline-content-info">
+            <span className="timeline-content-info-title">
+              <Server size="16px"/>&nbsp;
+              {props.company}
+            </span>
+            <span className="timeline-content-info-date">
+              <Calendar size="16px"/>&nbsp;
+              {props.date}
+            </span>
+          </div>
+          <p>{props.text}</p>
+          <ul className="timeline-content-skills">
+            {props.technologies.map((obj) => (
+              <li>{obj}</li>
+            ))}
+          </ul>
         </div>
-    </li>
+      </div>
+
   );
 };
 
