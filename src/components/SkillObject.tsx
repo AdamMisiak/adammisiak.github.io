9dea2d28469a7add605e50e92c724302fe13f4fa
Adam Misiak <adammisiak3@gmail.com>
2021-09-11 20:36:40 +0200

    Added tooltips for skills

--- src/components/SkillObject.tsx
+++ src/components/SkillObject.tsx
@@ -1,5 +1,5 @@
 import '../styles/Skills.css'
-
+import Tooltip from '@material-ui/core/Tooltip';
 import React from "react";
 import {StyledIcon} from '@styled-icons/styled-icon'
 
@@ -12,8 +12,10 @@ interface SkillObjectProps {
 
 const SkillObject: React.FunctionComponent<SkillObjectProps> = (props: SkillObjectProps) => {
   return (
-    <div title={props.name} data-aos="flip-up" data-aos-duration="1500" className={"skills-element " + (props.name)}>
-      {props.icon}
+    <div data-aos="flip-up" data-aos-duration="1500" className={"skills-element " + (props.name)}>
+      <Tooltip title={props.name[0].toUpperCase() + props.name.slice(1)}>
+        {props.icon}
+      </Tooltip>
     </div>
   );
 };
